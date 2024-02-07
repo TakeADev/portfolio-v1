4a1a44be8119e0cae42d50da86b1445570d5afc8
TakeADev <chanceconwaydev@gmail.com>
2024-02-07 09:58:05 -0500

    Create typing effect

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { SiTypescript } from 'react-icons/si'
 import { FaHtml5 } from 'react-icons/fa'
@@ -9,33 +9,53 @@ import { SiExpress } from 'react-icons/si'
 import { FaChevronDown } from 'react-icons/fa'
 
 const Hero = () => {
-  const [firstLine, setFirstLine] = useState('')
+  const [pinkText, setPinkText] = useState('')
+  const [pinkIndex, setPinkIndex] = useState(0)
+  const [whiteText, setWhiteText] = useState('')
+  const [whiteIndex, setWhiteIndex] = useState(0)
+  const [greenText, setGreenText] = useState('')
+  const [greenIndex, setGreenIndex] = useState(0)
+  const [greyText, setGreyText] = useState('')
+  const [greyIndex, setGreyIndex] = useState(0)
 
-  const hi = 'Hi, '
-
-  const timeout = (char) => {
-    setTimeout(() => {
-      setFirstLine(firstLine + char)
-    }, 1000)
+  const loopText = (
+    index: number,
+    indexSetter: Dispatch<SetStateAction<number>>,
+    textTarget: string,
+    currentText: string,
+    textSetter: Dispatch<SetStateAction<string>>
+  ) => {
+    if (index < textTarget.length) {
+      const timeout = setTimeout(() => {
+        textSetter(currentText + textTarget[index])
+        indexSetter(index + 1)
+      }, 50)
+      return () => clearTimeout(timeout)
+    }
   }
+  useEffect(() => {
+    const text1 = 'Hi, '
+    const text2 = "I'm "
+    const text3 = 'Chance Conway'
+    const text4 = 'Front End React/TypeScript Developer'
 
-  useEffect(() => { }, [hi])
-
-  console.log(firstLine)
+    loopText(pinkIndex, setPinkIndex, text1, pinkText, setPinkText)
+    pinkText === text1 && loopText(whiteIndex, setWhiteIndex, text2, whiteText, setWhiteText)
+    whiteText === text2 && loopText(greenIndex, setGreenIndex, text3, greenText, setGreenText)
+    greenText === text3 && loopText(greyIndex, setGreyIndex, text4, greyText, setGreyText)
+  }, [pinkIndex, pinkText, whiteIndex, whiteText, greenText, greenIndex, greyText, greyIndex])
 
   return (
     <div className='w-full h-screen '>
       <div className='pt-24 lg:pt-[20vh] mx-auto text-center lg:mx-20 lg:text-left'>
         <h1 className='text-7xl lg:text-9xl '>
-          <span className='text-[#C5A9BE]'>Hi, {firstLine}</span>
-          <span>I'm </span>
+          <span className='text-[#C5A9BE]'>{pinkText}</span>
+          <span>{whiteText}</span>
           <br />
-          <span className='text-[#A8DB94]'>Chance Conway</span>
+          <span className='text-[#A8DB94]'>{greenText}</span>
         </h1>
         <div className='w-3/4 mx-auto lg:w-full'>
-          <h2 className='text-gray-500 text-2xl lg:text-4xl mt-10'>
-            Front End React/TypeScript Developer
-          </h2>
+          <h2 className='text-gray-500 text-2xl lg:text-4xl mt-10'>{greyText}</h2>
         </div>
         <Link to={`#`}>
           <div
