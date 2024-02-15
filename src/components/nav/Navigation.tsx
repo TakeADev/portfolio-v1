c9c4ed5e18b2590e94feed5908e9ec6ba0ae7ec8
TakeADev <chanceconwaydev@gmail.com>
2024-02-15 10:29:05 -0500

    Add linkdin/github icons. Hover animations

--- src/components/nav/Navigation.tsx
+++ src/components/nav/Navigation.tsx
@@ -1,8 +1,10 @@
 import { useState, useContext } from 'react'
 
 import { MdOutlineMenu } from 'react-icons/md'
+import { FaLinkedinIn } from 'react-icons/fa'
+import { FiGithub } from 'react-icons/fi'
 
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 
 import { AnimationContext } from '../../contexts/AnimationContext'
 
@@ -29,18 +31,28 @@ const Navigation = () => {
         <nav
           className={`${!menuIsOpen && 'hidden '} ${toggleSlide && 'slide-left '}flex flex-col text-right bg-[] lg:flex lg:flex-row `}
         >
-          <NavLink to={`#`} className={`mr-10 mt-10`}>
+          <NavLink to={`#`} className={`mr-10 mt-10 ml-10 grow-on-hover`}>
             <span>Projects</span>
           </NavLink>
-          <NavLink to={`#`} className={`mr-10 mt-10`}>
+          <NavLink to={`#`} className={`mr-10 mt-10 grow-on-hover`}>
             <span>About</span>
           </NavLink>
           <NavLink
             to={`#`}
-            className={`mr-10 mt-8 mb-10 ml-10 p-2 border-2 border-[#7390A3] lg:-ml-2`}
+            className={`mr-10 mt-8 mb-10 ml-10 p-2 border-2 border-[#7390A3] lg:-ml-2 hover:bg-slate-900`}
           >
             <span>Contact Me</span>
           </NavLink>
+          <Link to={'https://www.linkedin.com/in/chance-conway-5b687828b/'} target='_blank'>
+            <div className='bg-[#7390a3] mr-10 mt-7 p-3 rounded-full grow-on-hover bg-opacity-50'>
+              <FaLinkedinIn className='text-4xl ' />
+            </div>
+          </Link>
+          <Link to={'https://github.com/TakeADev'} target='_blank'>
+            <div className='bg-[#7390a3] mr-10 mt-7 p-3 rounded-full grow-on-hover bg-opacity-50'>
+              <FiGithub className='text-4xl ' />
+            </div>
+          </Link>
         </nav>
       </div>
     )
