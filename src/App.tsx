f3866b4b111365c19a524ef89091440854134145
TakeADev <chanceconwaydev@gmail.com>
2024-02-15 10:28:22 -0500

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import './App.css'
 import { BrowserRouter as Router } from 'react-router-dom'
+
 import Navigation from './components/nav/Navigation'
 import Hero from './components/hero/Hero'
 import About from './components/sections/About'
 import AnimationContextProvider from './contexts/AnimationContext'
 import Projects from './components/sections/projects/Projects'
+
+import './App.css'
 
 function App() {
   return (
