1460f7523ab511c3cb5c17fa94718544a23f918c
TakeADev <chanceconwaydev@gmail.com>
2024-02-15 10:30:08 -0500

    Only shows after type animation is finished

--- src/components/sections/projects/Projects.tsx
+++ src/components/sections/projects/Projects.tsx
@@ -1,20 +1,37 @@
+import { useContext } from 'react'
+
+import { AnimationContext } from '../../../contexts/AnimationContext'
+
 import ProjectCard from './ProjectCard'
+
 import mnuscScreenshot from '../../../assets/Screenshot 2024-02-05 120853.png'
+import blogZoneScreenshot from '../../../assets/Screenshot 2024-02-14 121129.png'
 
 const Projects = () => {
+  const { typeAnimationIsPlaying } = useContext(AnimationContext)
+
   return (
-    <>
-      <h1 className='text-3xl text-center mb-10'>Projects</h1>
-      <div className='flex'>
-        <ProjectCard
-          title='MNUFC Social Club'
-          imgSrc={mnuscScreenshot}
-          imgAlt='Minnesota United Social Club Screenshot'
-          linkToCode='https://github.com/TakeADev/mnufc?tab=readme-ov-file'
-          linkToPreview='https://minnesotaunitedsocialclub.onrender.com'
-        />
-      </div>
-    </>
+    !typeAnimationIsPlaying && (
+      <>
+        <h1 className='text-4xl text-center mb-16'>Projects</h1>
+        <div className='flex w-3/4 mx-auto'>
+          <ProjectCard
+            title='MNUFC Social Club'
+            imgSrc={mnuscScreenshot}
+            imgAlt='Minnesota United Social Club Screenshot'
+            linkToCode='https://github.com/TakeADev/mnufc?tab=readme-ov-file'
+            linkToPreview='https://minnesotaunitedsocialclub.onrender.com'
+          />
+          <ProjectCard
+            title='Blog Zone'
+            imgSrc={blogZoneScreenshot}
+            imgAlt='Blog Zone Screenshot'
+            linkToCode='https://github.com/TakeADev/blog-app'
+            linkToPreview='https://blogzone.onrender.com'
+          />
+        </div>
+      </>
+    )
   )
 }
 
