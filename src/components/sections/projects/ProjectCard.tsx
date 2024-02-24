2eaf790507577c3998c3a65be2a0a579be738d0b
TakeADev <chanceconwaydev@gmail.com>
2024-02-24 10:52:12 -0500

    Projects will only render if it matchest the active tags

--- src/components/sections/projects/ProjectCard.tsx
+++ src/components/sections/projects/ProjectCard.tsx
@@ -1,59 +1,96 @@
-import { FunctionComponent } from 'react'
+import { FunctionComponent, useContext } from 'react'
 
 import { Link } from 'react-router-dom'
 
 import { FaGithub } from 'react-icons/fa'
 import { MdPreview } from 'react-icons/md'
 
+import { TagsContext } from '../../../contexts/TagsContext'
+
+import Tag from './Tag'
+
 import '../../../animations/animations.css'
 
 interface IProjectCardProps {
   title: string
   desc: string
+  projectTags: Array<string>
   imgSrc: string
   imgAlt: string
   linkToCode: string
   linkToPreview: string
+  addedClasses?: string
 }
 
 const ProjectCard: FunctionComponent<IProjectCardProps> = ({
   title,
   desc,
+  projectTags,
   imgSrc,
   imgAlt,
   linkToCode,
   linkToPreview,
 }) => {
-  // TODO: Impliment tags. Each product is tagged with technologies. User able to filter by tag
+  const { activeTags } = useContext(TagsContext)
+
+  const checkTags = (active: string[], project: string[]) => {
+    if (active.length < 1) return true
+    type TagMapValue = {
+      [key: string]: string
+    }
+    const projectTagMap: TagMapValue = {}
+
+    for (const tag of project) {
+      projectTagMap[tag] = tag
+    }
+
+    let hasTags = false
+
+    for (const tag of active) {
+      if (projectTagMap[tag]) {
+        hasTags = true
+      } else return (hasTags = false)
+    }
+    return hasTags
+  }
+
   return (
-    <div className='w-full lg:w-3/4 flex flex-col mx-auto bg-black bg-opacity-30 border border-[#7390A3]'>
-      <div className='flex w-full mx-auto mt-10'>
-        <div className='mx-10'>
-          <h2 className='text-4xl my-5'>{title}</h2>
-          <p className='text-lg'>{desc}</p>
+    checkTags(activeTags, projectTags) && (
+      <div
+        className={`w-full flex flex-col mx-auto bg-black bg-opacity-30 border border-[#7390A3] fade-in-fast`}
+      >
+        <div className='flex w-full mx-auto mt-10'>
+          <div className='mx-10'>
+            <h2 className='text-4xl my-5'>{title}</h2>
+            <p className='text-lg'>{desc}</p>
+          </div>
+        </div>
+        <div className='flex w-full p-10'>
+          <img
+            src={imgSrc}
+            alt={imgAlt}
+            className=' top-0 w-full object-cover border border-[#7390A3] rounded-md'
+          />
+        </div>
+        <div className='w-full flex flex-wrap -mt-2 mb-10 text-black ml-8 '>
+          {projectTags && projectTags.map((tag: string) => <Tag tagValue={tag} isActive={false} />)}
+        </div>
+        <div>
+          <div className='w-1/2 text-[#262338] mx-auto text-center py-2 mb-10 rounded-lg bg-[#C5A9BE] grow-on-hover'>
+            <Link to={linkToCode} className='text-3xl w-1/2' target='_blank'>
+              <FaGithub className='inline mr-5 -mt-1' />
+              <span className=''>Code</span>
+            </Link>
+          </div>
+          <div className='w-1/2 text-[#262338] mx-auto text-center py-2 mb-10 rounded-lg bg-[#A8DB94] grow-on-hover'>
+            <Link to={linkToPreview} className='text-3xl' target='_blank'>
+              <MdPreview className='inline mr-5 -mt-1' />
+              <span className=''>Preview</span>
+            </Link>
+          </div>
         </div>
       </div>
-      <div className='flex w-full p-10'>
-        <img
-          src={imgSrc}
-          className=' top-0 w-full object-cover border border-[#7390A3] rounded-md'
-        />
-      </div>
-      <div>
-        <div className='w-1/2 text-[#262338] mx-auto text-center py-2 mb-10 rounded-lg bg-[#C5A9BE]'>
-          <Link to={linkToCode} className='text-3xl' target='_blank'>
-            <FaGithub className='inline mr-5 -mt-1' />
-            <span className=''>Code</span>
-          </Link>
-        </div>
-        <div className='w-1/2 text-[#262338] mx-auto text-center py-2 mb-10 rounded-lg bg-[#A8DB94]'>
-          <Link to={linkToPreview} className='text-3xl' target='_blank'>
-            <MdPreview className='inline mr-5 -mt-1' />
-            <span className=''>Preview</span>
-          </Link>
-        </div>
-      </div>
-    </div>
+    )
   )
 }
 
