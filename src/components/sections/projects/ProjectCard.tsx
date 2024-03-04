ba9e9155442626325b618344b9a0d88f9e4d5d43
TakeADev <chanceconwaydev@gmail.com>
2024-03-04 18:02:41 -0500

    Change margins so text is not so condensed on small viewports

--- src/components/sections/projects/ProjectCard.tsx
+++ src/components/sections/projects/ProjectCard.tsx
@@ -60,13 +60,13 @@ const ProjectCard: FunctionComponent<IProjectCardProps> = ({
         className={`w-full flex flex-col mx-auto lg:mb-8 bg-black bg-opacity-30 border-t border-b lg:border border-[#7390A3] fade-in-fast lg:grid lg:grid-cols-2 lg:items-center lg:py-28 px-10`}
       >
         <div className='flex w-full mx-auto mt-0 '>
-          <div className='mx-5 lg:mr-10'>
+          <div className='lg:mr-10'>
             <h2 className='text-4xl lg:text-5xl my-5 lg:mb-16'>{title}</h2>
             <p className='lg:text-lg lg:leading-tight'>{desc}</p>
           </div>
         </div>
         <div className='lg:ml-10'>
-          <div className='flex w-full p-5'>
+          <div className='flex w-full mb-3'>
             <Link to={linkToPreview} target='_blank'>
               <img
                 src={imgSrc}
@@ -75,7 +75,7 @@ const ProjectCard: FunctionComponent<IProjectCardProps> = ({
               />
             </Link>
           </div>
-          <div className='w-full flex flex-wrap -mt-2 mb-10 text-black ml-8 pr-8'>
+          <div className='w-full flex flex-wrap -mt-2 mb-10 text-black pr-8'>
             {projectTags &&
               projectTags.map((tag: string) => {
                 const isActive = activeTags.find((foundTag) => tag === foundTag) ? true : false
@@ -83,13 +83,17 @@ const ProjectCard: FunctionComponent<IProjectCardProps> = ({
               })}
           </div>
           <div className='grid grid-cols-2'>
-            <Link to={linkToCode} className='text-xl md:text-2xl w-4/5 mx-auto' target='_blank'>
+            <Link to={linkToCode} className='text-xl md:text-2xl w-11/12 mx-auto' target='_blank'>
               <div className='w-full text-[#262338] mx-auto text-center py-2 mb-6 rounded-lg bg-[#C5A9BE] grow-on-hover'>
                 <FaGithub className='inline mr-2 -mt-1' />
                 <span className=''>Code</span>
               </div>
             </Link>
-            <Link to={linkToPreview} className='text-xl md:text-2xl w-4/5 mx-auto' target='_blank'>
+            <Link
+              to={linkToPreview}
+              className='text-xl md:text-2xl w-11/12 mx-auto'
+              target='_blank'
+            >
               <div className='w-full text-[#262338] mx-auto text-center py-2 mb-6 rounded-lg bg-[#A8DB94] grow-on-hover'>
                 <MdPreview className='inline mr-2 -mt-1' />
                 <span className=''>Preview</span>
