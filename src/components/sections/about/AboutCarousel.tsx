e4d904c111326da7a8bf5d7e5c25a5ec1e42856a
TakeADev <chanceconwaydev@gmail.com>
2024-03-04 18:00:56 -0500

    Style about carousel and add animations for changing slides

--- src/components/sections/about/AboutCarousel.tsx
+++ src/components/sections/about/AboutCarousel.tsx
@@ -1,38 +1,74 @@
+import { useEffect, useRef, useState } from 'react'
+import { CSSTransition, SwitchTransition } from 'react-transition-group'
+
 import { MdKeyboardArrowRight, MdKeyboardArrowLeft } from 'react-icons/md'
 import { BsDash } from 'react-icons/bs'
 
 import '../../../animations/animations.css'
 
 const AboutCarousel = () => {
+  const [position, setPosition] = useState(0)
+  const [leftOrRight, setLeftOrRight] = useState('carousel-slide-left')
+
+  const nodeRef = useRef(null)
+
+  const slides = [
+    { title: 'Languages', list: ['TypeScript', 'Javascript/ECMAScript', 'HTML5', 'CSS3'] },
+    {
+      title: 'Frontend Frameworks',
+      list: ['React', 'Bootstrap', 'TailwindCSS'],
+    },
+    { title: 'Backend / Databases', list: ['Express', 'Firebase', 'mongoDB'] },
+    { title: 'Runtime / Package Managers', list: ['node', 'npm', 'yarn', 'bun'] },
+    { title: 'Version Control', list: ['git', 'GitHub'] },
+  ]
+
+  const nextSlide = () => {
+    setLeftOrRight('carousel-slide-left')
+    position < 4 && setPosition(position + 1)
+  }
+
+  const previousSlide = () => {
+    setLeftOrRight('carousel-slide-right')
+    position > 0 && setPosition(position - 1)
+  }
+
+  useEffect(() => {
+    console.log(position)
+  }, [position])
+
   return (
-    <div className='mt-10 relative overflow-x-hidden'>
-      <div className='flex w-full'>
-        <div className='mt-32 text-4xl text-gray-600 cursor-pointer w-1/12 h-10'>
-          <MdKeyboardArrowLeft />
+    <div className='h-full'>
+      <div className='flex h-[90%] overflow-x-hidden'>
+        <div className='w-1/12 flex items-center z-10 cursor-pointer' onClick={previousSlide}>
+          <MdKeyboardArrowLeft className='inline text-3xl' />
         </div>
-        <div className='flex-col'>
-          <div className='text-center mb-5'>
-            <span className='text-3xl '>Languages</span>
-          </div>
-          <div className='w-10/12 '>
-            <ul className='list-disc text-xl leading-loose ml-16 '>
-              <li>Typescript</li>
-              <li>Javascript/ECMAScript</li>
-              <li>HTML5</li>
-              <li>CSS3</li>
-            </ul>
-          </div>
+        <div className='w-10/12 flex flex-col justify-center'>
+          <SwitchTransition>
+            <CSSTransition key={position} timeout={300} classNames={leftOrRight}>
+              <div className={''} ref={nodeRef}>
+                <div className='text-center text-2xl mb-5'>
+                  <span>{slides[position].title}</span>
+                </div>
+                <div className='ml-10 lg:ml-16 text-lg'>
+                  <ul className='list-disc'>
+                    {slides[position].list.map((item) => (
+                      <li>{item}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </CSSTransition>
+          </SwitchTransition>
         </div>
-        <div className='mt-32 ml-5 text-4xl text-gray-600 cursor-pointer w-1/12 h-10'>
-          <MdKeyboardArrowRight />
+        <div className='w-1/12 flex items-center cursor-pointer z-10' onClick={nextSlide}>
+          <MdKeyboardArrowRight className='text-3xl' />
         </div>
       </div>
-      <div className='absolute bottom-0 text-center w-full'>
-        <div className='mx-auto w-1/4 text-3xl'>
-          <BsDash className='inline ' />
-          <BsDash className='inline text-gray-600' />
-          <BsDash className='inline text-gray-600' />
-        </div>
+      <div className='h-[10%] text-xl flex items-center justify-center'>
+        <BsDash className='inline' />
+        <BsDash className='inline' />
+        <BsDash className='inline' />
       </div>
     </div>
   )
